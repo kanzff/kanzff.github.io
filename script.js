480b997cbeb5f8e07f869107069a275755646c63
kanzff <rivakanzf@gmail.com>
2020-11-19 19:38:35 +0700

    Final Project 0

--- script.js
+++ script.js
@@ -3,34 +3,47 @@
 const char = document.getElementById("char")
 const obs = document.getElementById("obs")
 
-//function initialize() {
-    function jump() {
-        if (char.classList != "jump") {
-            char.classList.add("jump");
+alert('Are you Geh? click ok to find out')
+
+const counter = document.getElementById("counter")
+ 
+let skor = 0
+//let countD = `Your Score : ${skor}`
+function increment() {
+    counter.textContent = 'Your Score : ' + skor;
+    skor++;
+}
+setInterval('increment()', 1000);
+
+
+function jump() {
+    if (char.classList != "jump") {
+        char.classList.add("jump");
+    
+        setTimeout(function () {
+            char.classList.remove("jump");
+        }, 300)
+    }
+} 
+
+
+let isCollision = setInterval(function () {
+    let charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"));
+    let obsLeft = parseInt(window.getComputedStyle(obs).getPropertyValue("left"));
+    //let posAwal = parseint(window.getComputedStyle(obs).getPropertyValue("left"));
+
+    //console.log(obsLeft)
+
+    if (obsLeft < 230 && obsLeft > 130 && charTop >= 290) {
+        //console.log('nabrak')
+        let endMessage = `You Are GEH!!!`
+        alert(endMessage)
+        skor = 0
         
-            setTimeout(function () {
-                char.classList.remove("jump");
-            }, 300)
-        }
-    } 
+    }
+}, 10);
 
+document.addEventListener("keydown", function (event) {
+    jump()
+});
 
-    let isCollision = setInterval(function () {
-        let charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"));
-        let obsLeft = parseInt(window.getComputedStyle(obs).getPropertyValue("left"));
-        //let posAwal = parseint(window.getComputedStyle(obs).getPropertyValue("left"));
-
-        //console.log(obsLeft)
-
-        if (obsLeft < 180 && obsLeft > 130 && charTop >= 340) {
-            //console.log('nabrak')
-            let endMessage = `Game Over`
-            alert(endMessage)
-            
-        }
-    }, 10);
-
-    document.addEventListener("keydown", function (event) {
-        jump()
-    });
-//}
