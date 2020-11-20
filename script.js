3617ff36fa08cb19f690fb56db0b5082f180c172
kanzff <rivakanzf@gmail.com>
2020-11-20 14:54:41 +0700

    Final Project 0

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 
+const counter = document.getElementById("counter") 
 
-const char = document.getElementById("char")
-const obs = document.getElementById("obs")
+//var bgm = new Audio('audio/bgm.mp3')
+var auGeh = new Audio('audio/geh.mp3')
+var mjump = new Audio('audio/mjump.mp3')
+mjump.volume = 0.03;
 
-alert('Are you Geh? click ok to find out')
-
-const counter = document.getElementById("counter")
- 
 let skor = 0
 //let countD = `Your Score : ${skor}`
+
+
 function increment() {
     counter.textContent = 'Your Score : ' + skor;
     skor++;
 }
-setInterval('increment()', 1000);
+
+(function initialize(){
+    var c = document.getElementById('obs');
+    var d = document.getElementById("start")
+    function addAnim() {
+        setInterval('increment()', 500);
+        c.classList.add('ricrush')
+        // remove the listener, no longer needed
+        d.removeEventListener('mouseover', addAnim);
+    };
+
+     //listen to mouseover for the container
+    d.addEventListener('mouseover', addAnim);
+    
+})();
+
+const char = document.getElementById("char")
+const obs = document.getElementById("obs")
+
+//alert('Are you Geh? click ok to find out')
+
+/*const counter = document.getElementById("counter") 
+
+let skor = 0
+//let countD = `Your Score : ${skor}`
+
+
+function increment() {
+    counter.textContent = 'Your Score : ' + skor;
+    skor++;
+} */
+
+//if () {
+//setInterval('increment()', 1000);
+//}
+
 
 
 function jump() {
@@ -36,6 +72,7 @@ let isCollision = setInterval(function () {
 
     if (obsLeft < 230 && obsLeft > 130 && charTop >= 290) {
         //console.log('nabrak')
+        auGeh.play()
         let endMessage = `You Are GEH!!!`
         alert(endMessage)
         skor = 0
@@ -44,6 +81,7 @@ let isCollision = setInterval(function () {
 }, 10);
 
 document.addEventListener("keydown", function (event) {
+    mjump.play()
     jump()
 });
 
